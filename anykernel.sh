a52e18f7d1af52330282cc51456c0faec1fb8ce8
Ren <89468157+Shirayuki39@users.noreply.github.com>
2025-04-26 11:10:50 +0800

    anykernel.sh: enable no_magisk_check

--- anykernel.sh
+++ anykernel.sh
@@ -24,6 +24,7 @@ BLOCK=boot
 IS_SLOT_DEVICE=1
 RAMDISK_COMPRESSION=auto
 PATCH_VBMETA_FLAG=auto
+NO_MAGISK_CHECK=1
 
 # Import functions/variables and setup patching - see for reference (DO NOT REMOVE)
 . tools/ak3-core.sh
